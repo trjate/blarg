2faf54d9b03cc40668a966bae020a22ac1e8f24b
Brit Butler <redline6561@gmail.com>
2015-06-08 10:55:05 -0400

    Add PostTags model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608144913) do
+ActiveRecord::Schema.define(version: 20150608145410) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
