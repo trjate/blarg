3b81e1c3ad5a226ac47bbb9e0f446244be1cb22e
Brit Butler <redline6561@gmail.com>
2015-06-09 09:58:28 -0400

    Add index page to the site.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
+  def index
+    page = params[:page] || 1
+    @posts = self.get_page(page)
+  end
+
   def show
     @post = Post.find(params[:id])
     render :show
+  end
+
+  private
+  def get_page(n)
+    page_offset = (n - 1) * 10
+    Post.order(written_at: :desc).offset(page_offset).limit(10)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   get 'posts/:id', to: 'posts#show'
+  get 'posts', to: 'posts#index'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
