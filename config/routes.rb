eb3ba9828c7561fd19f688094478b94e49118604
Brit Butler <redline6561@gmail.com>
2015-06-09 11:09:49 -0400

    Add links to my site.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id', to: 'posts#show', as: 'post'
   get 'posts', to: 'posts#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
