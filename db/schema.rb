45d755d0793437d4200a5254ba2296a24eeb2669
Brit Butler <redline6561@gmail.com>
2015-06-16 10:08:55 -0400

    Add Comments on posts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615162256) do
+ActiveRecord::Schema.define(version: 20150616140621) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
